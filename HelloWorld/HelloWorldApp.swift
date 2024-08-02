5c16d1ca709a03feb8f56c41c01958523d0e9809
Luke Clifford <luke@firesky.com.au>
2024-08-02 18:09:42 +1000

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Luke Clifford on 2/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
